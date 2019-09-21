1dd7d021386792fc8639990733299b69d7a40920
Manuel Fernandez <mfernandezyt@gmail.com>
2019-09-21 11:43:49 +0000

    Added contact create action to save to db / fixed typos

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190904215106) do
+ActiveRecord::Schema.define(version: 20190920194049) do
 
-  create_table "contactos", force: :cascade do |t|
-    t.string   "nombre"
+  create_table "contacts", force: :cascade do |t|
+    t.string   "name"
     t.string   "email"
-    t.text     "comentarios"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.text     "comments"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
